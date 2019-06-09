74da843555ed7b09bf2385e70f374e5e86c4429d
Mkhululi Mtshemla <201402344@student.uj.ac.za>
2019-06-10 01:20:40 +0200

    edit device and user ui

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  amount: string = "active";
+  balance: number = 100;
   constructor() { }
 
   ngOnInit() {
